0f7f44d04763ae2361b20eb894c4ef6674ba1cce
Jooeun Park <civbf1@gmail.com>
2024-01-08 20:04:12 -0800

    add messages

--- chatweb/base/views.py
+++ chatweb/base/views.py
@@ -4,15 +4,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
 def loginPage(request):
+    page ='login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -28,13 +33,28 @@ def loginPage(request):
         else:
             messages.error(request, "Username or password not exist")
     
-    context ={}
+    context ={'page': page}
     return render(request, 'login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerPage(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occurred during registration")
+
+    return render(request, 'login_register.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -51,7 +71,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'rooms': room}
+    room_messages = room.message_set.all().order_by('-created')
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    participants = room.participants.all()
+
+    context = {'room': room, 'room_messages':room_messages, 'participants': participants}
     return render(request, 'room.html', context)
 
 @login_required(login_url='login')
